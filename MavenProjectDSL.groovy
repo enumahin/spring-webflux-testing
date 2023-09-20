a63250892a67b81e7bb55be5b4606d3d06283d45
enumahin <enumahin@gmail.com>
2023-09-20 14:06:51 +0100

    Added DSL Script

---
+++ MavenProjectDSL.groovy
@@ -0,0 +1,16 @@
+job('First-Java-Compilation-DSL'){
+    description("First Maven job generated bt the DSL on ${new Date()}")
+    scm{
+        git("git@github.com:enumahin/spring-webflux-testing.git", 'master')
+    }
+    trigger{
+        scm("* * * * *")
+    }
+    steps{
+        maven("clean package")
+    }
+    publishers {
+        // archive the war file generated
+        archiveArtifacts('../*.jar')
+    }
+}
